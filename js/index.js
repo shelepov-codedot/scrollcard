f056e3a5712b78ff1f401f75e405d7aab422e87b
shelepov-codedot <shelepov.codedot@gmail.com>
2021-01-26 15:08:31 +0300

    try to fix

--- js/index.js
+++ js/index.js
@@ -1,28 +1,17 @@
-const scrollGrid = (selector) => {
-    if (selector) {
-        const bodyHeight = document.body.offsetHeight
-        const mainHeight = document.querySelector('main').offsetHeight
-        const cards = document.querySelector(selector)
-        const transY = (window.pageYOffset / (mainHeight - bodyHeight)) * -100;
-
-        cards.style.setProperty('--scrollGrid', transY + '%');
-    } else {
-        console.log('selector not founded');
-    }
-}
-
-window.addEventListener("scroll", () => scrollGrid('.grid-cards'));
-
-/* const scrollTest = (selector) => {
+const scrollTest = (selector) => {
     if (selector) {
         const mainElem = document.querySelector(selector)
         const bodyHeight = document.body.offsetHeight
-        const transY = (window.pageYOffset / (mainElem.offsetHeight - bodyHeight)) * -100
+        const transY = ((window.pageYOffset - 2500) / (mainElem.offsetHeight - bodyHeight)) * -80;
 
         mainElem.style.setProperty("--scrollGrid", transY + "%")
+
     } else {
         console.log('selector not founded');
     }
 }
 
-window.addEventListener('scroll', () => scrollTest('.main')); */
+window.addEventListener('scroll', () => {
+    if (pageYOffset >= 2500)
+        window.addEventListener('scroll', () => scrollTest('.main'));
+})
