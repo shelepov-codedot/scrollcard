4b021219079cb74ccaecfa3cb057d725c989dcd7
shelepov-codedot <shelepov.codedot@gmail.com>
2021-01-26 12:16:16 +0300

    Add adaptation

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
-const scrollTest = (selector = 'main') => {
-    if (selector == 'main') {
-        let bodyHeight = document.body.offsetHeight
-        let mainHeight = document.querySelector(selector)
-        let transY = (window.pageYOffset / (mainHeight.offsetHeight - bodyHeight)) * -100
+const scrollGrid = (selector) => {
+    if (selector) {
+        const bodyHeight = document.body.offsetHeight
+        const mainHeight = document.querySelector('main').offsetHeight
+        const cards = document.querySelector(selector)
+        const transY = (window.pageYOffset / (mainHeight - bodyHeight)) * -100;
 
-        mainHeight.style.setProperty("--scrollGrid", transY + "%")
+        cards.style.setProperty('--scrollGrid', transY + '%');
     } else {
         console.log('selector not founded');
     }
 }
 
-window.addEventListener('scroll', () => scrollTest('main'));
+window.addEventListener("scroll", () => scrollGrid('.grid-cards'));
+
+/* const scrollTest = (selector) => {
+    if (selector) {
+        const mainElem = document.querySelector(selector)
+        const bodyHeight = document.body.offsetHeight
+        const transY = (window.pageYOffset / (mainElem.offsetHeight - bodyHeight)) * -100
+
+        mainElem.style.setProperty("--scrollGrid", transY + "%")
+    } else {
+        console.log('selector not founded');
+    }
+}
+
+window.addEventListener('scroll', () => scrollTest('.main')); */
